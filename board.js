f161683dfaa9f77475f9ca3fb5affed34fd7bd4b
chanhui <chanhui.yu.cnu@gmail.com>
2021-06-22 21:20:14 +0900

    Feat : reserving, lending function

--- board.js
+++ board.js
@@ -3,6 +3,83 @@ function addEbook(){
 	
 }
 
+//책 대출
+function lendingEbook(isbn){
+	$.ajax({
+		url:'lendingEbook.php',
+		type: 'post',
+		data:{
+			mode : 1
+		}
+	}).done(function(data){
+		if (data == "OK"){
+			var flag = confirm("현재 대출이 가능합니다.\n대출하시겠습니까?");
+			if (flag){
+				$.ajax({
+					url:'lendingEbook.php',
+					type: 'post',
+					data : {
+						mode : 2,
+						isbn : isbn
+					}
+				}).done(function(data){
+					if (data == "OK"){
+						alert("대출이 완료되었습니다.");
+						location.reload();
+					}
+					else{
+						alert("Error");
+					}
+				});
+			}
+			else{
+				alert("취소하셨습니다.");
+			}
+		}
+		else if (data == "NO"){
+			alert("최대 3권까지 대출이 가능합니다.");
+		}
+	});
+}
+
+//책 예약
+function reservingEbook(ISBN){
+	$.ajax({
+		url:'reservingEbook.php',
+		type: 'post',
+		data : {
+			mode : 1
+		}
+	}).done(function(data){
+		if (data == "OK"){
+			var flag = confirm("현재 예약이 가능합니다.\n예약하시겠습니까?");
+			if (flag){
+				$.ajax({
+					url : 'reservingEbook.php',
+					type:'post',
+					data:{
+						mode : 2,
+						ISBN : ISBN
+					}
+				}).done(function(data){
+					if (data == "OK"){
+						alert("예약이 완료되었습니다.");
+					}
+					else{
+						alert("Error");
+					}
+				});
+			}
+			else{
+				alert("취소하셨습니다.");
+			}
+		}
+		else if(data == "NO"){
+			alert("최대 3권까지 예약 가능합니다.");
+		}
+	});
+}
+
 function logout(){
 	$.ajax({
 		url:'logout.php',
