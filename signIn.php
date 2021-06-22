2e961c18c5926c3e19984d18bb14710a7174e926
chanhui <chanhui.yu.cnu@gmail.com>
2021-06-22 18:24:28 +0900

    Fix : seperate upper case and lower case alphabet

--- signIn.php
+++ signIn.php
@@ -1,6 +1,7 @@
 <?php
-$email = strtoupper($_POST['email']);
-$pw = strtoupper($_POST['pw']);
+
+$email = $_POST['email'];
+$pw = $_POST['pw'];
 
 $dbuser="D201902721";
 $dbpass="hancihu0079";
@@ -13,14 +14,16 @@ oci_execute($stmt);
 while($row = oci_fetch_assoc($stmt))
 {
 	if ($row['EMAIL'] == $email && $row['PASSWD'] == $pw){
-		echo "ACCESS";
 		session_start();
 		$_SESSION['cno'] = $row['CNO'];
 		$_SESSION['name'] = $row['NAME'];
+		$_SESSION['mode'] = 1; //EbookList 정렬 default 값
+		echo "ACCESS";
 		exit;
 	}
 }
 echo "DENY";
+
 // 오라클 접속 닫기 
 oci_free_statement($stmt);
 
