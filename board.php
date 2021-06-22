772ff05133060be82d6ce98f7d120d9123d6bf09
chanhui <chanhui.yu.cnu@gmail.com>
2021-06-22 17:14:03 +0900

    delete board.html

--- board.php
+++ board.php
@@ -1,35 +1,52 @@
-<?php
-//session_start();
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-$dbuser="D201902721";
-$dbpass="hancihu0079";
+    <link rel="stylesheet" href="board.js">
+    <body>
+    <div class="container">
+			<div class="row" id="header">
+				<p>
+          <!-- 이름, 마이페이지, 로그아웃-->
+					<span><?php session_start(); echo $_SESSION['name']."님 환영합니다."; ?></span>
+          <span id="myPage-btn"><a class="btn btn-dark" href="./signIn.html">마이 페이지</a></span>
+					<span id="sign-btn"><a class="btn btn-dark" href="./signIn.html">로그아웃</a></span>
+         
+				</p>
+			</div>
 
-$conn = oci_connect($dbuser,$dbpass,'localhost/XE','AL32UTF8');
+			<div class="row">
+        <table class="table table-hover">
+				  <thead>
+						<tr>
+							<th>No.</th>
+							<th>제목</th>
+							<th>출판사</th>
+							<th>출판연도</th>
+              <th colspan='2'>
+              <div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">정렬 ▼</button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item">ISBN</a><br>
+                  <a class="dropdown-item">책 제목</a><br>
+                  <a class="dropdown-item">출판사</a><br>
+                  <a class="dropdown-item">출판 연도</a>
+                </div>
+              </div>
+              </th>
+              <!-- 관리자인 경우 활성화--> 
+              <th><button class="btn btn-default" onclick="addBook()" disabled>+</button></th>
+						</tr>
+					</thead>
 
-//$_SESSION["conn"] = $conn;
+				  <tbody id="EbookList">
+            
+			  	</tbody>
+		  	</table>
+			</div>
+    </body>
 
-if(!$conn) {
-  echo "No Connection ".oci_error();
-  exit;
-} else {
- echo "Connect Success!";
-}
-
-$query = 'select * from CUSTOMER';
-
-$stmt = oci_parse($conn,$query);
-oci_execute($stmt);
-
-
-while($row = oci_fetch_assoc($stmt))
-{
-    print_r($row);
-}
-
-
-// 오라클 접속 닫기 
-oci_free_statement($stmt);
-
-// 오라클에서 로그아웃 
-oci_close($conn); 
-?>
+  </head>
+</html>
