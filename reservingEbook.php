1c472cb4dd88a7ed4bd3512ca213b81ae6aee633
chanhui <chanhui.yu.cnu@gmail.com>
2021-06-22 20:43:25 +0900

    Feat : check user can reserve, insert data to reserve table

---
+++ reservingEbook.php
@@ -0,0 +1,44 @@
+<?php
+/*
+mode 1 : 예약이 가능한지 확인
+mode 2 : 해당 도서 예약
+*/
+
+$dbuser="D201902721";
+$dbpass="hancihu0079";
+$conn = oci_connect($dbuser,$dbpass,'localhost/XE','AL32UTF8');
+
+$isbn = $_POST['ISBN'];
+$cno = $_POST['cno'];
+
+if ($_POST['mode'] == 1){
+
+	//한 고객은 최대 3권까지 예약 가능
+	$query = "select CNO, count(CNO) C from reserve group by CNO";
+	$stmt = oci_parse($conn, $query);
+	oci_execute($stmt);
+	while ($row = oci_fetch_assoc($stmt)){
+		if($row['CNO'] == $cno){
+			if ($row['C'] >= 3){
+				echo "NO";
+				exit;
+			}
+			else{
+				echo "OK";
+				exit;
+			}
+		}
+	}
+}
+
+else if($_POST['mode'] == 2){
+	$query = "insert into reserve values(${isbn},${cno},TO_DATE(SYSDATE,'YYYY/MM/DD'))";
+	$stmt = oci_parse($conn, $query);
+	oci_execute($stmt);
+}
+
+oci_free_statement($stmt);
+oci_close($conn);
+
+echo "OK";
+?>
